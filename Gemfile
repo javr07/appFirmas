f305706e2a2ba88bc6868b1fafdc3e1a4ae75691
Unknown <ppjavr@gmail.com>
2018-12-03 01:24:37 -0600

    Sidebar y detalles
    Propuesta de sidebar (sin enlaces). Corrección de alertas. Redirección para necesitar siempre login. Inicio de manejo de cuentas (adm y user).

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.0'
+gem 'toastr-rails'
 gem 'rails-i18n'
 gem 'carrierwave'
 gem 'bootstrap'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   resources :documents
-  root to: 'documents#index'
+  
   devise_for :users
+  authenticate :user do
+  	root to: 'documents#index', as: :authenticated_root
+  end
+  root to: 'documents#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
